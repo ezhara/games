43f22f78cbb29664bc4470743c83d941e978792e
@ezhara0405 <ezhara0405@gmail.com>
2017-08-24 02:02:06 +0300

    Card styling bugs

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -31,7 +31,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [360, 270]
+    process resize_to_fill: [360, 235]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
